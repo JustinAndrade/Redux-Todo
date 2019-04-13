8a075697746c0467f422ed4c240d581ca16b4e9e
JustinAndrade <j.andrade.john@gmail.com>
2019-04-13 15:08:04 -0400

    Modal and Stretch Completed

--- todo/src/components/TodoList.js
+++ todo/src/components/TodoList.js
@@ -3,11 +3,24 @@ import { connect } from 'react-redux';
 
 import { addTodo, toggleCompleted } from '../actions'
 
+import './todolist.css'
+
+
 class TodoList extends React.Component {
     state = {
         newValue: ''
     }
     
+    openModal = e => {
+        const modal = document.getElementById('todo-modal');
+        modal.style.display = 'block'
+    }
+
+    closeModal = e => {
+        const closeModal = document.getElementById('todo-modal')
+        closeModal.style.display = 'none'
+    }
+
     handleChanges = e => {
         this.setState({
             newValue: e.target.value
@@ -16,6 +29,8 @@ class TodoList extends React.Component {
 
     addValue = e => {
         e.preventDefault();
+        const closeModal = document.getElementById('todo-modal');
+        closeModal.style.display = 'none';
         this.props.addTodo(this.state.newValue)
         this.setState({ newValue: '' })
     }
@@ -28,22 +43,31 @@ class TodoList extends React.Component {
         return (
             <>
                 <div className='todo-list-wrapper'>
+                    <button onClick={this.openModal} id='modalBtn' className='button'>Add a Task</button>
+                    <div id='todo-modal'>
+                        <div className='todo-content'>
+                            <span onClick={this.closeModal} className='closeBtn'>&times;</span>
+                            <form>
+                                <input
+                                    type='tex\'
+                                    value={this.state.todo}
+                                    onChange={this.handleChanges}
+                                    placeholder='Add a New Task'
+                                    onSubmit={this.addValue}
+                                />
+                            <button className='addBtn' onClick={this.addValue}>Add Task</button>
+                            </form>
+                        </div>
+                    </div>
                     {this.props.todos.map((todo, index) => (
+                        <div className='todo'>
                         <h4 onClick={() => this.toggleCompleted(todo.id)} key={todo.id}>
                             {todo.value}
                             {todo.completed && <i className="fas fa-check-circle"/>}
                         </h4>
+                        </div>
                     ))}
                 </div>
-                <form>
-                    <input
-                        type='text'
-                        value={this.state.todo}
-                        onChange={this.handleChanges}
-                        placeholder='Add a New Task'
-                    />
-                </form>
-                <button onClick={this.addValue}>Add Task</button>
             </>
         )
     }
